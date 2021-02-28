1a4779f5578e8eec409a7a71127a6f0bd5e14f6b
datagram1 <36324711+datagram1@users.noreply.github.com>
2021-02-28 11:53:04 +0000

    Upgrade BASE_DIR - add symlink support resolves directory not found on mac

--- manage.sh
+++ manage.sh
@@ -2,7 +2,13 @@
 
 export LANG=C
 
-BASE_DIR="$(dirname -- "`readlink -f -- "$0"`")"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+BASE_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 cd -- "$BASE_DIR"
 set -e
